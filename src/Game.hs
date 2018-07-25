ba249457993408065d425c0cfbf78fe3b75a2541
Mateusz Curylo <mateusz.curylo@gl05654m.gamesys.corp>
2018-07-25 12:55:04 +0100

    Freer monad for game

--- src/Game.hs
+++ src/Game.hs
@@ -8,6 +8,7 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE LambdaCase #-}
 
 module Game
     () where
@@ -17,6 +18,7 @@ import           Data.Text          (Text)
 import qualified Data.Text          as T
 import           Data.IntMap        (IntMap)
 import qualified Data.IntMap.Strict as Map
+import           Control.Arrow      (first)
 import           Control.Lens       (makeLenses)
 import           Control.Monad.Freer
 import           Control.Monad.Freer.Error
@@ -44,6 +46,8 @@ data family ObjectId a
 data instance ObjectId Avatar = AvatarId Int deriving Show  
 data instance ObjectId Obstacle = ObstacleId Int deriving Show 
 
+-- Object for physic implementation
+
 data Object2d = MkObject2d {
     _objectCenter :: Point
   , _objectShape :: Shape
@@ -51,18 +55,22 @@ data Object2d = MkObject2d {
   , _objectVelocity :: Point
 } deriving (Show)
 
+-- Avatars for Players (and NPCs?)
+
 data Avatar = MkAvatar {
     _avatarObject :: !Object2d
 } deriving (Show)
+
+-- Noninteractive game objects 
 
 data Obstacle = MkObstacle {
     _obstacleObject :: !Object2d
 } deriving (Show)
 
+-- Stroing it all
+
 type Avatars = IntMap Avatar
 type Obstacles = IntMap Obstacle
-
-
 
 data GameWorld = MkGameWorld {
     _avatars :: Avatars
@@ -87,14 +95,38 @@ startingAvatar (AvatarId n) = Map.singleton n (MkAvatar $ MkObject2d newAvatarPo
 
 gameWorld avatarName = MkGameWorld (startingAvatar avatarName) basicObstacles
 
-
 ----------------------------------------------------------------------------------
 -- Actions
 ----------------------------------------------------------------------------------
 
+-- Actions accepted from the Player / AI
+
+data ActionReq where
+  AccelerateReq   :: Point -> ActionReq
+  RotateReq       :: Angle -> ActionReq
+
+-- Responses to the Player 
+
+data ActionResp where
+  MoveResp        :: ObjectId a -> Point -> ActionResp
+  RotateResp      :: ObjectId a -> Angle -> ActionResp
+
+-- Actual game actions 
+
 data Action m where
-  Move   :: ObjectId a -> Point -> Action ()
-  Rotate :: ObjectId a -> Angle -> Action ()
+  AvatarAct       :: ObjectId Avatar -> ActionReq -> Action () 
+  Accelerate      :: ObjectId a -> Point -> Action ()
+  Move            :: ObjectId a -> Point -> Action ()
+  Rotate          :: ObjectId a -> Angle -> Action ()
+  RunSimulation   :: Action ()
+
+-- Sending game actions to freer monad
+
+avatarAct :: Member Action effs => ObjectId Avatar -> ActionReq -> Eff effs ()
+avatarAct avaId = send . AvatarAct avaId
+
+accelerate :: Member Action effs => ObjectId a -> Point -> Eff effs ()
+accelerate objId = send . Accelerate objId 
 
 move :: Member Action effs => ObjectId a -> Point -> Eff effs ()
 move objId = send . Move objId 
@@ -102,13 +134,28 @@ move objId = send . Move objId
 rotate :: Member Action effs => ObjectId a -> Angle -> Eff effs ()
 rotate objId = send . Rotate objId
 
-interpretAction :: Action v -> Eff '[State GameWorld] v
-interpretAction (Move a p) = pure ()
-interpretAction (Rotate a p) = pure ()
+runSimulation :: Member Action effs => Eff effs ()
+runSimulation = send RunSimulation
 
-sequenceActions :: [Action ()] -> Eff '[State GameWorld] [()]
-sequenceActions = mapM interpretAction 
+-- For now just accept everything user throws at you
 
-runGameActions :: [Action ()] -> GameWorld -> GameWorld
-runGameActions actions gameWorld = snd . run $ (runState gameWorld $ sequenceActions actions)
+interpretAvatarAction :: ObjectId Avatar -> ActionReq -> Eff '[Action, Writer [ActionResp], State GameWorld] ()
+interpretAvatarAction avaId (AccelerateReq p) = accelerate avaId p
+interpretAvatarAction avaId (RotateReq r) = rotate avaId r
 
+-- The actual implementation of game
+
+interpretAction :: Eff '[Action, Writer [ActionResp], State GameWorld] v -> Eff '[Writer [ActionResp], State GameWorld] v
+interpretAction = interpret (\case
+  AvatarAct avaId p -> interpretAction (interpretAvatarAction avaId p)
+  Accelerate objId p -> return ()
+  Move objId p -> return ()
+  Rotate objId p -> return ()
+  RunSimulation -> return ())
+
+-- Function to run a sequence of actions in game and recalculate the simulation
+
+runGameActions :: [Action ()] -> GameWorld -> ([ActionResp], GameWorld)
+runGameActions actions world = first snd
+                               $ run . runState world . runWriter 
+                               $ traverse interpretAction (map send $ actions ++ [RunSimulation])
