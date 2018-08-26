7479e208290e68f562a627934ae1e99998c08436
Mateusz Curylo <mhcurylo@gmail.com>
2018-08-26 08:53:42 +0100

    Tests working

--- test/Arbitrary.hs
+++ test/Arbitrary.hs
@@ -5,13 +5,19 @@ module Arbitrary where
 import Test.QuickCheck.Arbitrary
 import Generic.Random
 import Objects
+import Data.Hashable
+import Data.HashMap.Strict
 
 instance Arbitrary Angle where
   arbitrary = genericArbitrary uniform
-instance Arbitrary Shape where
+instance Arbitrary Shape where 
   arbitrary = genericArbitrary uniform
 instance Arbitrary Point where
   arbitrary = genericArbitrary uniform
+instance (Arbitrary v, Arbitrary k, Hashable k, Ord k) => Arbitrary (HashMap k v) where
+  arbitrary = do
+   (kv :: [(k, v)]) <- arbitrary
+   return $ fromList kv 
 instance Arbitrary Object2d where
   arbitrary = genericArbitrary uniform
 instance Arbitrary Avatar where
--- test/ObjectsTests.hs
+++ test/ObjectsTests.hs
@@ -1,7 +1,35 @@
 module ObjectsTests where
 
+import Arbitrary()
 import Test.Tasty.QuickCheck
+import Data.Aeson
 import Objects
 
 prop_additionCommutative :: Point -> Point -> Bool
-prop_additionCommutative a b = a + b == b + a
+prop_additionCommutative a b = add a b == add b a
+
+correctJSON :: (Eq a, FromJSON a, ToJSON a) => a -> Bool
+correctJSON a = (Just a) == (decode . encode) a
+
+prop_correctJSON_Point :: Point -> Bool
+prop_correctJSON_Point = correctJSON
+
+prop_correctJSON_Angle :: Angle -> Bool
+prop_correctJSON_Angle = correctJSON
+
+prop_correctJSON_Shape :: Shape -> Bool
+prop_correctJSON_Shape = correctJSON
+
+prop_correctJSON_Object2d :: Object2d -> Bool
+prop_correctJSON_Object2d = correctJSON
+
+prop_correctJSON_Avatar :: Avatar -> Bool
+prop_correctJSON_Avatar = correctJSON
+
+prop_correctJSON_Obstacle :: Obstacle -> Bool
+prop_correctJSON_Obstacle = correctJSON
+
+prop_correctJSON_GameWorld :: GameWorld -> Bool
+prop_correctJSON_GameWorld = correctJSON
+
+
